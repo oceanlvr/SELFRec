01325ab677c33d132c9e22a99813cc04f9f20809
oceanlvr <657531018@qq.com>
2023-11-08 18:31:16 +0800

    add swavloss

--- model/graph/SwAVGCL.py
+++ model/graph/SwAVGCL.py
@@ -5,7 +5,7 @@ from base.graph_recommender import GraphRecommender
 
 from util.sampler import next_batch_pairwise
 from base.torch_interface import TorchGraphInterface
-from util.loss_torch import bpr_loss, l2_reg_loss, InfoNCE
+from util.loss_torch import bpr_loss, l2_reg_loss
 import faiss
 import wandb
 from sklearn.cluster import KMeans
@@ -19,42 +19,36 @@ class SwAVGCL(GraphRecommender):
         super(SwAVGCL, self).__init__(conf, training_set, test_set)
         self.model = LGCN_Encoder(
             self.data,
-            self.config['embedding_size'],
-            self.config['model_config.num_layers']
+            self.config["embedding_size"],
+            self.config['model_config.eps'],
+            self.config["model_config.num_layers"],
         )
 
-    def SwaVNCE_loss(self, initial_emb, user_idx, item_idx):
-        user_emb, item_emb = torch.split(
-            initial_emb, [self.data.user_num, self.data.item_num])
+    def e_step(self):
+        user_embeddings = self.model.embedding_dict["user_emb"].detach().cpu().numpy()
+        item_embeddings = self.model.embedding_dict["item_emb"].detach().cpu().numpy()
+        self.user_centroids, self.user_2cluster = self.run_kmeans(user_embeddings)
+        self.item_centroids, self.item_2cluster = self.run_kmeans(item_embeddings)
 
-        # Normalize embeddings
-        user_emb = F.normalize(user_emb, dim=1, p=2)
-        item_emb = F.normalize(item_emb, dim=1, p=2)
-
-        # Calculate similarity matrices with temperature
-        user_user_sim = user_emb[user_idx] @ user_emb.t() / \
-            self.config['model_config.temperature']
-        item_item_sim = item_emb[item_idx] @ item_emb.t() / \
-            self.config['model_config.temperature']
-        user_item_sim = user_emb[user_idx] @ item_emb[item_idx].t() / \
-            self.config['model_config.temperature']
-
-        # Getting the pseudo labels by using the highest similarity
-        _, user_pseudo_labels = user_user_sim.max(dim=1)
-        _, item_pseudo_labels = user_item_sim.max(dim=1)
-
-        # Calculate cross-entropy loss
-        swav_nce_loss_user = F.cross_entropy(user_user_sim, user_pseudo_labels)
-        swav_nce_loss_item = F.cross_entropy(item_item_sim, item_pseudo_labels)
-        swav_nce_loss_user_item = F.cross_entropy(
-            user_item_sim, item_pseudo_labels)
-
-        swav_nce_loss = swav_nce_loss_user + swav_nce_loss_item + swav_nce_loss_user_item
-        return swav_nce_loss
+    def run_kmeans(self, x):
+        """Run K-means algorithm to get k clusters of the input tensor x"""
+        kmeans = faiss.Kmeans(
+            d=self.config["embedding_size"],
+            k=self.config["model_config.num_clusters"],
+            gpu=True,
+        )
+        kmeans.train(x)
+        cluster_cents = kmeans.centroids
+        _, I = kmeans.index.search(x, 1)
+        # convert to cuda Tensors for broadcast
+        centroids = torch.Tensor(cluster_cents).cuda()
+        node2cluster = torch.LongTensor(I).squeeze().cuda()
+        return centroids, node2cluster
 
     def SwaVNCE_loss_1(self, initial_emb, user_idx, item_idx):
         user_emb, item_emb = torch.split(
-            initial_emb, [self.data.user_num, self.data.item_num])
+            initial_emb, [self.data.user_num, self.data.item_num]
+        )
 
         # Normalize embeddings
         user_emb = F.normalize(user_emb, dim=1, p=2)
@@ -71,83 +65,145 @@ class SwAVGCL(GraphRecommender):
         # Calculate cross-entropy loss
         swav_nce_loss_user = F.cross_entropy(user_user_sim, user_pseudo_labels)
         swav_nce_loss_item = F.cross_entropy(user_item_sim, item_pseudo_labels)
-        swav_nce_loss = self.config['model_config.proto_reg'] * (swav_nce_loss_user + swav_nce_loss_item)
+        swav_nce_loss = self.config["model_config.proto_reg"] * (
+            swav_nce_loss_user + swav_nce_loss_item
+        )
         return swav_nce_loss
 
+    def swavloss(self, user_idx, item_idx, temperature=0.1):
+        u_idx = torch.unique(torch.Tensor(user_idx).type(torch.long)).cuda()
+        i_idx = torch.unique(torch.Tensor(item_idx).type(torch.long)).cuda()
+        user_view_1, item_view_1 = self.model(perturbed=True)
+        user_view_2, item_view_2 = self.model(perturbed=True)
 
-    def SwaVNCE_loss_2(self, initial_emb, user_idx, item_idx):
-            user_emb, item_emb = torch.split(
-                initial_emb, [self.data.user_num, self.data.item_num])
+        # Compute cluster assignment distributions for both views using Sinkhorn-Knopp
+        q_user_view_1 = self.sinkhorn_knopp(user_view_1 / temperature)
+        q_user_view_2 = self.sinkhorn_knopp(user_view_2 / temperature)
+        q_item_view_1 = self.sinkhorn_knopp(item_view_1 / temperature)
+        q_item_view_2 = self.sinkhorn_knopp(item_view_2 / temperature)
 
-            # Normalize embeddings
-            user_emb = F.normalize(user_emb, dim=1, p=2)
-            item_emb = F.normalize(item_emb, dim=1, p=2)
+        # Compute loss - Cross-entropy between soft assignments and features of the other view
+        loss_user = -torch.mean(
+            torch.sum(
+                q_user_view_1 * F.log_softmax(user_view_2 / temperature, dim=1),
+                dim=1,
+            )
+        )
+        loss_user += -torch.mean(
+            torch.sum(
+                q_user_view_2 * F.log_softmax(user_view_1 / temperature, dim=1),
+                dim=1,
+            )
+        )
 
-            # Perform K-Means clustering to get pseudo labels
-            kmeans_user = KMeans(n_clusters=self.num_clusters).fit(
-                user_emb.cpu().detach().numpy())
-            kmeans_item = KMeans(n_clusters=self.num_clusters).fit(
-                item_emb.cpu().detach().numpy())
+        loss_item = -torch.mean(
+            torch.sum(
+                q_item_view_1 * F.log_softmax(item_view_2 / temperature, dim=1),
+                dim=1,
+            )
+        )
+        loss_item += -torch.mean(
+            torch.sum(
+                q_item_view_2 * F.log_softmax(item_view_1 / temperature, dim=1),
+                dim=1,
+            )
+        )
 
-            user_pseudo_labels = torch.tensor(kmeans_user.labels_)[
-                                            user_idx].to(initial_emb.device)
-            item_pseudo_labels = torch.tensor(kmeans_item.labels_)[
-                                            item_idx].to(initial_emb.device)
+        # 注意这里 proto_nce_loss_item 前面没有去加这个 alpha 系数
+        proto_nce_loss = self.config["model_config.proto_reg"] * (loss_user + loss_item)
+        return proto_nce_loss
 
-            # Calculate similarity matrices
-            user_user_sim = user_emb[user_idx] @ user_emb.t()
-            user_item_sim = user_emb[user_idx] @ item_emb[item_idx].t()
+    def sinkhorn_knopp(self, log_Q, num_iters=3, epsilon=1e-3):
+        """
+        将输入矩阵log_Q转换为双随机形式。
 
-            # Calculate cross-entropy loss
-            swav_nce_loss_user = F.cross_entropy(user_user_sim, user_pseudo_labels)
-            swav_nce_loss_item = F.cross_entropy(user_item_sim, item_pseudo_labels)
+        :param log_Q: 输入矩阵的对数形式。
+        :param num_iters: Sinkhorn-Knopp算法迭代次数。
+        :param epsilon: 为了数值稳定性而加的小量。
+        :return: 调整后的双随机矩阵。
+        """
+        with torch.no_grad():
+            device = log_Q.device  # Get the device from log_Q
+            Q = torch.exp(log_Q)  # Convert log probabilities to probabilities.
+            sum_Q = torch.sum(Q)
+            Q /= sum_Q  # Normalize Q
 
-            swav_nce_loss = swav_nce_loss_user + swav_nce_loss_item
-            return swav_nce_loss
+            r = torch.ones(Q.shape[0], device=device) / Q.shape[0]  # Move r to the device of log_Q
+            c = torch.ones(Q.shape[1], device=device) / Q.shape[1]  # Move c to the device of log_Q
+
+            curr_sum = torch.sum(Q, dim=1)
+
+            for _ in range(num_iters):
+                # Update rows
+                Q *= (r / curr_sum).view(-1, 1)
+                Q /= torch.sum(Q, dim=0, keepdim=True)
+                Q /= torch.sum(Q, dim=1, keepdim=True)
+
+                # Update columns
+                curr_sum = torch.sum(Q, dim=1)
+                if torch.max(torch.abs(curr_sum - r)) < epsilon:
+                    # Break if the change is smaller than epsilon for numerical stability
+                    break
+
+            # Final normalization to avoid numerical issues
+            Q *= (r / curr_sum).view(-1, 1)
+            Q /= torch.sum(Q, dim=0, keepdim=True)
+
+            return torch.log(Q + epsilon)
 
     def train(self):
         model = self.model.cuda()
         optimizer = torch.optim.Adam(
-            model.parameters(), lr=self.config['learning_rate'])
-        for epoch in range(self.config['num_epochs']):
-
-            for index, batch in enumerate(next_batch_pairwise(self.data, self.config['batch_size'])):
+            model.parameters(), lr=self.config["learning_rate"]
+        )
+        for epoch in range(self.config["num_epochs"]):
+            for index, batch in enumerate(
+                next_batch_pairwise(self.data, self.config["batch_size"])
+            ):
                 user_idx, pos_idx, neg_idx = batch
 
                 # brp 损失 LGCN 部分 emb_list 是 all_emb
-                rec_user_emb, rec_item_emb, emb_list = model()
-
-                # 第一层初始的 GNN emb
-                initial_emb = emb_list[0]
-                # 第 2*N 层 GNN emb
-                context_emb = emb_list[self.config['model_config.hyper_layers']*2]
-                # 这部分是layer位置的对比损失
+                rec_user_emb, rec_item_emb = model()
 
                 # 推荐的brp损失+l2损失
-                user_emb, pos_item_emb, neg_item_emb = rec_user_emb[
-                    user_idx], rec_item_emb[pos_idx], rec_item_emb[neg_idx]
-                rec_loss = bpr_loss(user_emb, pos_item_emb, neg_item_emb) + l2_reg_loss(
-                    self.config['lambda'], user_emb, pos_item_emb, neg_item_emb) / self.config['batch_size']
-
-                cl_loss = self.SwaVNCE_loss_1(
-                    initial_emb, user_idx, pos_idx
+                user_emb, pos_item_emb, neg_item_emb = (
+                    rec_user_emb[user_idx],
+                    rec_item_emb[pos_idx],
+                    rec_item_emb[neg_idx],
                 )
+                rec_loss = (
+                    bpr_loss(user_emb, pos_item_emb, neg_item_emb)
+                    + l2_reg_loss(
+                        self.config["lambda"], user_emb, pos_item_emb, neg_item_emb
+                    )
+                    / self.config["batch_size"]
+                )
+
+                cl_loss = self.swavloss(user_idx, pos_idx)
 
                 batch_loss = rec_loss + cl_loss
                 optimizer.zero_grad()
                 batch_loss.backward()
                 optimizer.step()
-                wandb.log({
-                    'epoch': epoch + 1,
-                    'batch_loss': batch_loss.item(),
-                    'rec_loss': rec_loss.item(),
-                    'cl_loss': cl_loss.item(),
-                })
+                wandb.log(
+                    {
+                        "epoch": epoch + 1,
+                        "batch_loss": batch_loss.item(),
+                        "rec_loss": rec_loss.item(),
+                        "cl_loss": cl_loss.item(),
+                    }
+                )
 
                 if index % 100 == 0:
                     print(
-                        'training:', epoch + 1, 'batch', index, 'rec_loss:',
-                        rec_loss.item(), 'cl_loss', cl_loss.item()
+                        "training:",
+                        epoch + 1,
+                        "batch",
+                        index,
+                        "rec_loss:",
+                        rec_loss.item(),
+                        "cl_loss",
+                        cl_loss.item(),
                     )
             with torch.no_grad():
                 self.user_emb, self.item_emb, _ = model()
@@ -165,34 +221,34 @@ class SwAVGCL(GraphRecommender):
 
 
 class LGCN_Encoder(nn.Module):
-    def __init__(self, data, emb_size, n_layers):
+    def __init__(self, data, emb_size, eps, n_layers):
         super(LGCN_Encoder, self).__init__()
         self.data = data
-        self.latent_size = emb_size
-        self.layers = n_layers
+        self.eps = eps
+        self.emb_size = emb_size
+        self.n_layers = n_layers
         self.norm_adj = data.norm_adj
         self.embedding_dict = self._init_model()
-        self.sparse_norm_adj = TorchGraphInterface.convert_sparse_mat_to_tensor(
-            self.norm_adj).cuda()
+        self.sparse_norm_adj = TorchGraphInterface.convert_sparse_mat_to_tensor(self.norm_adj).cuda()
 
     def _init_model(self):
         initializer = nn.init.xavier_uniform_
         embedding_dict = nn.ParameterDict({
-            'user_emb': nn.Parameter(initializer(torch.empty(self.data.user_num, self.latent_size))),
-            'item_emb': nn.Parameter(initializer(torch.empty(self.data.item_num, self.latent_size))),
+            'user_emb': nn.Parameter(initializer(torch.empty(self.data.user_num, self.emb_size))),
+            'item_emb': nn.Parameter(initializer(torch.empty(self.data.item_num, self.emb_size))),
         })
         return embedding_dict
 
-    def forward(self):
-        ego_embeddings = torch.cat(
-            [self.embedding_dict['user_emb'], self.embedding_dict['item_emb']], 0)
-        all_embeddings = [ego_embeddings]
-        for k in range(self.layers):
-            ego_embeddings = torch.sparse.mm(
-                self.sparse_norm_adj, ego_embeddings)
-            all_embeddings += [ego_embeddings]
-        lgcn_all_embeddings = torch.stack(all_embeddings, dim=1)
-        lgcn_all_embeddings = torch.mean(lgcn_all_embeddings, dim=1)
-        user_all_embeddings = lgcn_all_embeddings[:self.data.user_num]
-        item_all_embeddings = lgcn_all_embeddings[self.data.user_num:]
+    def forward(self, perturbed=False):
+        ego_embeddings = torch.cat([self.embedding_dict['user_emb'], self.embedding_dict['item_emb']], 0)
+        all_embeddings = []
+        for k in range(self.n_layers):
+            ego_embeddings = torch.sparse.mm(self.sparse_norm_adj, ego_embeddings)
+            if perturbed:
+                random_noise = torch.rand_like(ego_embeddings).cuda()
+                ego_embeddings += torch.sign(ego_embeddings) * F.normalize(random_noise, dim=-1) * self.eps
+            all_embeddings.append(ego_embeddings)
+        all_embeddings = torch.stack(all_embeddings, dim=1)
+        all_embeddings = torch.mean(all_embeddings, dim=1)
+        user_all_embeddings, item_all_embeddings = torch.split(all_embeddings, [self.data.user_num, self.data.item_num])
         return user_all_embeddings, item_all_embeddings, all_embeddings
